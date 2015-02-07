2d4722ab685c52b4697431eb4345cb76da18b6a9
Janice Leung <jjw1707@gmail.com>
2015-02-07 17:36:31 -0500

    added search form route.

--- senop/controller.py
+++ senop/controller.py
@@ -4,6 +4,10 @@ from . import app
 
 from flask import render_template
 
+@app.route('/search')
+def search_form():
+  return render_template('search_form.html')
+
 @app.route('/')
 def homepage():
   return render_template('index.html')
