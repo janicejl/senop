9320bd1955b7060e23a33736092b009319f56a27
tamblc <tamblc@rpi.edu>
2015-02-08 09:14:49 -0500

    gettin scripty

--- senop/static/js/main.js
+++ senop/static/js/main.js
@@ -41,7 +41,9 @@ function update(response){
 			str="";
 		});
 		$("#pos").html(newContent);
-
+		var href = $("#livefeed").href;
+		href = href.concat(results.search_term);
+		$("#livefeed").href = href;
 		// pBar.aria-valuenow =results.score*100;
 	} else {
 
