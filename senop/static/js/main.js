46f385b536c62f78b75a06c050ff8c51c3cffda0
tamblc <tamblc@rpi.edu>
2015-02-08 03:42:13 -0500

    js ish

--- senop/static/js/main.js
+++ senop/static/js/main.js
@@ -1,7 +1,20 @@
 function getResults() {
 	var tmp = document.getElementById("searchTerm");
 	var term = tmp.value;
+	var searchTerm='{"searchTerm": '+ term+'}'; 
+	$.ajax({
+		url:Flask.url_for('search',
+		dataType : 'json',
+		contentType : 'application/json; charset=UTF-8',
+		data : JSON.stringify(searchTerm),
+		type : 'POST',
+		//success : onStatusOK,
+		//failure : onStatusError
+		complete: function(response) {
+			if(response['responseJSON'][''])
 
+		})
+	})
 	alert(term);
 
 }
