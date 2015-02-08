213b378d7c6ca879575dc5d58f81408afe20884d
tamblc <tamblc@rpi.edu>
2015-02-08 08:05:31 -0500

    data

--- senop/static/js/main.js
+++ senop/static/js/main.js
@@ -1,3 +1,33 @@
+function checkEnter() {
+ var tmp = document.getElementById("searchTerm");
+
+    var keypressed = event.keyCode || event.which;
+    	console.log(keypressed);
+    if (keypressed == 13) {
+    	event.preventDefault();
+        getResults();
+    }
+}
+
+function update(response){
+	var results = response.responseJSON.results;
+	console.log(results);
+	var pBar = document.getElementById("mood");
+	if (results.score > 0) {
+		var valeur = results.score*100;
+		$('.progress-bar').css('width', valeur+'%').attr('aria-valuenow', valeur);
+		var newContent = "";
+		results.forEach() {
+
+		} 
+		$('.w-before').innerHTML
+		// pBar.aria-valuenow =results.score*100;
+	} else {
+
+	}
+
+}
+
 function getResults() {
   var tmp = document.getElementById("searchTerm");
   var term = tmp.value;
@@ -12,14 +42,8 @@ function getResults() {
     //success : onStatusOK,
     //failure : onStatusError
     complete: function(response) {
-            console.log(response);
+    		console.log(response);
+            update(response);
 		}
 	});
 }
-
-$('#searchTerm').keydown(function (event) {
-    var keypressed = event.keyCode || event.which;
-    if (keypressed == 13) {
-        getResults();
-    }
-});
