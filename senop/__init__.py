c474332e08283dd6131d24c2c090915e18a0d590
Janice Leung <jjw1707@gmail.com>
2015-02-08 03:08:40 -0500

    added flask js_glue

--- senop/__init__.py
+++ senop/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask
 
 import tweepy
 
+from flask_jsglue import JSGlue
+
 app = Flask(__name__, instance_relative_config = True)
 app.config.from_object('config.BaseConfiguration')
 app.config.from_pyfile('config.py', silent=True)
 
 twauth = tweepy.OAuthHandler(app.config['TWITTER_KEY'], app.config['TWITTER_SECRET'])
 twapi = tweepy.API(twauth)
+
+jsglue = JSGlue(app)
 
 import senop.controller
 
