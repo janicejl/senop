6d5230a29d18883548c8d4664a3043dbf1e20add
Janice Leung <jjw1707@gmail.com>
2015-02-08 07:07:12 -0500

    json

--- senop/static/js/main.js
+++ senop/static/js/main.js
@@ -1,20 +1,21 @@
 function getResults() {
-	var tmp = document.getElementById("searchTerm");
-	var term = tmp.value;
-	var searchTerm="{'searchTerm': '"+term+"'}"; 
-	$.ajax({
-		url:Flask.url_for('search'),
-		dataType : 'json',
-		contentType : 'application/json; charset=UTF-8',
-		data : JSON.stringify(searchTerm),
-		type : 'POST',
-		//success : onStatusOK,
-		//failure : onStatusError
-		complete: function(response) {
-			console.log(response);
+  var tmp = document.getElementById("searchTerm");
+  var term = tmp.value;
+  var searchTerm = {};
+  searchTerm['searchTerm'] = term;
+  $.ajax({
+    url:Flask.url_for('search'),
+    dataType : 'json',
+    contentType : 'application/json; charset=UTF-8',
+    data : JSON.stringify(searchTerm),
+    type : 'POST',
+    //success : onStatusOK,
+    //failure : onStatusError
+    complete: function(response) {
+            console.log(response);
 
-		}
-	});
-	alert(term);
+    }
+  });
+  alert(term);
 
 }
