0bee3c37ec7fe12f6a59a867bf35306b8d292427
Janice Leung <jjw1707@gmail.com>
2015-03-03 12:55:20 -0800

    Added new logging settings since we are moving it to heroku

--- senop/__init__.py
+++ senop/__init__.py
@@ -8,6 +8,13 @@ app = Flask(__name__, instance_relative_config = True)
 app.config.from_object('config.BaseConfiguration')
 app.config.from_pyfile('config.py', silent=True)
 
+# Heroku Logging settings. 
+import logging
+stream_handler = logging.StreamHandler()
+app.logger.addHandler(stream_handler)
+app.logger.setLevel(logging.INFO)
+app.logger.info('Senop started.')
+
 twauth = tweepy.OAuthHandler(app.config['TWITTER_KEY'], app.config['TWITTER_SECRET'])
 twapi = tweepy.API(twauth)
 
